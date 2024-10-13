982bc0406fa3af90bac26ed79df8f13408de40d5
Beatriz Pereira Espinheira <beatrizespinheiraa@gmail.com>
2024-10-13 08:36:41 -0300

    inicializa histórico de conversas no chat

--- hackathon-bemobi/src/components/Chat.tsx
+++ hackathon-bemobi/src/components/Chat.tsx
@@ -11,10 +11,16 @@ interface Message {
 }
 
 const Chat = () => {
-  const [messages, setMessages] = useState<Message[]>([
-    { id: 1, role: 'assistant', content: 'O Matheus! Percebi que você tem enfrentado algumas dificuldades com o pagamento da Smartfit no dia 1º de cada mês. Você gostaria de compartilhar o que está acontecendo? Estou aqui para ajudar!' },
-    { id: 2, role: 'user', content: 'Meu cartão vira na data de pagamento da smartfit, e como eu não tenho limite disponível, o valor não é debitado da minha conta, preciso pagar na maquininha na academia' }
-  ]);
+    const [messages, setMessages] = useState<Message[]>([
+        { id: 1, role: 'assistant', content: 'Oi! Eu sou a Iasmin, sua assistente virtual da Smart Gym 😄. Estou aqui para te ajudar com matrícula, escolher o melhor plano de treinos, gerenciar pagamentos e esclarecer dúvidas sobre os benefícios da academia. Precisa de ajuda com algum desses assuntos? 💪🏋️‍♂️💳' },
+        { id: 2, role: 'user', content: 'Olá meu nome é Matheus e estou tendo dificuldade com a data de pagamento no primeiro dia de cada mês.' },
+        { id: 3, role: 'assistant', content: 'Oi, Matheus! Entendi, você está tendo dificuldades com o pagamento no primeiro dia de cada mês. Se quiser, posso te ajudar a alterar a data de vencimento da sua mensalidade para um dia mais conveniente. 😊 Confirma para mim se deseja mesmo alterar a data, e me avise qual seria a nova data ideal para você! 👇' },
+        { id: 4, role: 'user', content: 'Eu queria alterar a data de pagamento para o dia 10 de cada mês, seria possível?' },
+        { id: 5, role: 'assistant', content: 'Certo, Matheus! Vou confirmar com você antes de prosseguir. 😊 Você deseja mesmo alterar a data de pagamento para o dia 10 de cada mês? Se sim, me avise e já farei o ajuste!' },
+        { id: 6, role: 'user', content: 'Confirmo.' },
+        { id: 7, role: 'assistant', content: 'Perfeito, Matheus! Estou alterando a data de pagamento para o dia 10 de cada mês. 💪📅 Se precisar de mais alguma coisa, é só me avisar! 😊' },
+      ]);
+
   const [input, setInput] = useState('');
 
   const handleSubmit = (e: FormEvent) => {
@@ -22,7 +28,7 @@ const Chat = () => {
     if (input.trim() !== '') {
       const newMessage: Message = {
         id: messages.length + 1,
-        role: 'assistant',
+        role: 'user',
         content: input,
       };
       setMessages([...messages, newMessage]);
